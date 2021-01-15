c4103e092ae2cc728cb9a6a404823d8627a3821f
ka2250 <katok2250@gmail.com>
2021-01-15 17:43:56 +0900

    header, event-formを作成

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import "./style.css"; // StyleCSS読み込み
 function App() {
   return (
     <div className="App">
-      <header>
-        <h4>creatEvent App</h4>
-        <p>using react hooks.</p>
-      </header>
+      <div className="container">
+        <header className="header">
+          <h4 className="header-title">creatEvent App</h4>
+          <p className="header-desc">using react hooks.</p>
+        </header>
+
+        <form className="event-form">
+          <div className="form-group">
+            <label for="input-event">create event :</label>
+            <input type="text" className="form-control" id="input-event" placeholder="input your event..." />
+            <button type="button" class="btn btn-light">Add</button>
+          </div>
+
+        </form>
+
+      </div>
     </div>
   );
 }
