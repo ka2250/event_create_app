80b5ec4189cb205135eb5e5ba28a4ae9060e4421
ka2250 <katok2250@gmail.com>
2021-01-18 00:06:22 +0900

    feat: style.cssを削除してスタイルをBootstrapで管理

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
 import 'bootstrap/dist/css/bootstrap.min.css';　// bootstrap読み込み
-import "./style.css"; // StyleCSS読み込み
 
 function App() {
+
   return (
     <div className="App">
       <div className="container">
-        <header className="header">
-          <h4 className="header-title">creatEvent App</h4>
-          <p className="header-desc">using react hooks.</p>
+
+        {/* ヘッダー */}
+        <header className="mt-5">
+          <h4>Creat Event App</h4>
+          <p>using react hooks.</p>
         </header>
 
-        <form className="event-form">
-          <div className="form-group">
-            <label for="input-event">create event :</label>
+        {/* フォーム */}
+        <form className="mt-4">
+          <label className="mr-2 font-weight-bold" for="input-event">create event</label>
+          <div class="d-flex justify-content-start">
             <input type="text" className="form-control" id="input-event" placeholder="input your event..." />
-            <button type="button" class="btn btn-light">Add</button>
+            <button type="button" class="btn btn-outline-secondary ml-3">Add</button>
           </div>
-
         </form>
 
+        {/* イベントリスト */}
+        <div className="event-list mt-5">
+          <div className="d-flex justify-content-between mb-2">
+            <h5 className="font-weight-bold">Events</h5>
+            <button type="button" class="btn btn-danger btn-sm">DELETE EVENTS</button>
+          </div>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th className="text-center">ID</th>
+                <th>Event Description</th>
+                <th className="text-center">Edit</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <th className="text-center">1</th>
+                <td>aaaaaaaaaaaaaaaaaa</td>
+                <td className="text-center">edit</td>
+              </tr>
+              <tr>
+                <th>2</th>
+                <td>aaaaaaaaaaaaaaaaaa</td>
+                <td className="text-center">edit</td>
+              </tr><tr>
+                <th className="text-center">3</th>
+                <td>aaaaaaaaaaaaaaaaaa</td>
+                <td className="text-center">edit</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+        {/* ログリスト */}
+        <div className="log-list mt-5">
+          <div className="d-flex justify-content-between mb-2">
+            <h5 className="font-weight-bold">Logs</h5>
+            <button type="button" class="btn btn-secondary btn-sm">DELETE LOGS</button>
+          </div>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th className="text-center">ID</th>
+                <th>Delete Event</th>
+                <th className="text-center">Operated</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <th className="text-center">1</th>
+                <td>aaaaaaaaaaaaaaaaaa</td>
+                <td className="text-center">2021/1/17</td>
+              </tr>
+              <tr>
+                <th className="text-center">2</th>
+                <td>aaaaaaaaaaaaaaaaaa</td>
+                <td className="text-center">2021/1/17</td>
+              </tr><tr>
+                <th className="text-center">3</th>
+                <td>aaaaaaaaaaaaaaaaaa</td>
+                <td className="text-center">2021/1/17</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
