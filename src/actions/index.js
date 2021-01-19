6b86e4122985f84ff60efad6b354ebff6b4e9e65
ka2250 <katok2250@gmail.com>
2021-01-20 02:06:26 +0900

    reducersで使われるアクションを定義

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+// eventReducer-Actions
+export const CREATE_EVENT = "CREATE_EVENT";
+export const DELETE_EVENT = "DELETE_EVENT";
+export const DELETE_ALL_EVENT = "DELETE_ALL_EVENT";
+
+// operationReducer-Actions
+export const ADD_OPERATION_LOG = "ADD_OPERATION_LOG";
+export const DELETE_ALL_OPERATION_LOGS = "DELETE_ALL_OPERATION_LOGS";
