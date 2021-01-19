40ed031345a65d5693a17ccc6b92def776d93059
ka2250 <katok2250@gmail.com>
2021-01-19 16:04:31 +0900

    コンポーネントのインポートを編集

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';　// bootstrap読み込み
 
-import FormHeader from "./components/FormHeader";
-import Events from './components/Events';
+import FormAndList from "./components/FormAndList";
 import OperatedLogs from "./components/OperatedLogs";
 
 function App() {
@@ -11,8 +10,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <FormHeader />
-        <Events />
+        <FormAndList />
         <OperatedLogs />
       </div>
     </div>
