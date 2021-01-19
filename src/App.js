18a4be2e4138832c03cc262fd0f8bb0eb72ff18b
ka2250 <katok2250@gmail.com>
2021-01-19 11:07:05 +0900

    コンポーネント名変更に伴いインポートも変更

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
+import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';　// bootstrap読み込み
 
-import Header from "./components/Header";
-import EventForm from './components/EventForm';
-import EventList from './components/EventList';
-import EventLogs from "./components/EventLogs";
+import FormHeader from "./components/FormHeader";
+import Events from './components/Events';
+import OperatedLogs from "./components/OperatedLogs";
 
 function App() {
 
   return (
     <div className="App">
       <div className="container">
-        <Header />
-        <EventForm />
-        <EventList />
-        <EventLogs />
+        <FormHeader />
+        <Events />
+        <OperatedLogs />
       </div>
     </div>
   );
