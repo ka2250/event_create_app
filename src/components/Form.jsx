338202b26d8f028dd30066eca679ba86bc13f7ab
ka2250 <katok2250@gmail.com>
2021-01-21 11:44:41 +0900

    FormAndList -> Formへとファイル名を変更

---
+++ src/components/Form.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from "react";
+import AppContext from "../contexts/AppContext";
+import { currentTime } from "../util";
+import { ADD_OPERATION_LOG } from "../actions";
+
+const Form = () => {
+
+    const { dispatch } = useContext(AppContext);
+    const [title, setTitle] = useState('');
+
+    const unClear = title === "";
+
+    const addEvent = e => {
+        e.preventDefault();
+        dispatch({
+            type: "CREATE_EVENT",
+            title
+        })
+        dispatch({
+            type: ADD_OPERATION_LOG,
+            description: `イベントを作成しました`,
+            operatedAt: currentTime()
+        })
+        setTitle('');
+    }
+
+    return (
+        <>
+
+            <form className="mt-4">
+                <label className="mr-2" htmlFor="input-event">create event : </label>
+                <div className="d-flex justify-content-around">
+                    <input
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        type="text"
+                        className="form-control"
+                        id="input-event"
+                        placeholder="input your event..."
+                    />
+                    <button
+                        disabled={unClear}
+                        type="button"
+                        className="btn btn-outline-secondary ml-3"
+                        onClick={addEvent}
+                    >Add</button>
+                </div>
+            </form>
+        </>
+    );
+}
+
+export default Form;
