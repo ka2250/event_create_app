2f06567cb72cc1091e9fb23c50a56a384cdfe434
ka2250 <katok2250@gmail.com>
2021-01-21 11:47:17 +0900

    インポート関係を整理しコードをフォーマット

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';　// bootstrap読み込み
 
-import FormAndList from "./components/FormAndList";
-import OperatedLogs from "./components/OperatedLogs";
+import AppContext from "./contexts/AppContext";
+import reducers from './reducers';
+
+import Header from "./components/Header";
+import Form from "./components/Form";
+import EventsList from './components/EventsList';
+import OperatedLogsList from "./components/OperatedLogsList";
+
+
 
 function App() {
+  const initialState = {
+    eventReducer: [],
+    operationReducer: [],
+  };
+  const [state, dispatch] = useReducer(reducers, initialState);
 
   return (
-    <div className="App">
-      <div className="container">
-        <FormAndList />
-        <OperatedLogs />
+    <AppContext.Provider value={{ state, dispatch }}>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Form />
+          <EventsList />
+          <OperatedLogsList />
+        </div>
       </div>
-    </div>
+    </AppContext.Provider>
   );
 }
 
