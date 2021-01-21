d7604c09538342d4095ed8b1be83ff2115b8a275
ka2250 <katok2250@gmail.com>
2021-01-21 11:46:19 +0900

    イベントが表示されるコンポーネント作成

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 
-const Event = () => {
-
+const Event = ({ event, index }) => {
     return (
-        <>
-
-        </>
+        <tr key={index}>
+            <th className="text-center">{index + 1}</th>
+            <td>{event.title}</td>
+            <td className="text-center">Edit</td>
+        </tr>
     );
 }
 
---
+++ src/components/EventsList.jsx
@@ -0,0 +1,60 @@
+import React, { useContext } from "react";
+import AppContext from "../contexts/AppContext";
+import Event from "./Event";
+import { currentTime } from "../util";
+import { ADD_OPERATION_LOG } from "../actions";
+
+const EventsList = () => {
+    const { state, dispatch } = useContext(AppContext);
+
+    const deleteAllEvent = e => {
+        e.preventDefault();
+        const result = window.confirm('すべてのイベントを削除しますか？');
+        if (result) {
+            dispatch({
+                type: "DELETE_ALL_EVENT",
+            })
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: "すべてのイベントを削除しました。",
+                operatedAt: currentTime()
+            })
+        }
+    }
+
+    return (
+        <>
+            <div className="event-list mt-5">
+                <div className="d-flex justify-content-between mb-2">
+                    <h5>Events</h5>
+                    <button
+                        type="button"
+                        className="btn btn-danger btn-sm"
+                        onClick={deleteAllEvent}
+                        disabled={state.length === 0}
+                    >DELETE EVENTS</button>
+                </div>
+            </div>
+
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th className="text-center">ID</th>
+                        <th>Description</th>
+                        <th className="text-center">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        state.eventReducer.map((event, index) => (
+                            <Event key={index} index={index} event={event} />
+                        ))
+                    }
+                </tbody>
+
+            </table>
+        </>
+    );
+}
+
+export default EventsList;
