492bb59fa5bf50fd9897a9f02a7152ed33d03c62
ka2250 <katok2250@gmail.com>
2021-01-21 15:59:27 +0900

    themeReducerのアクションを定義

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,8 @@ export const DELETE_ALL_EVENT = "DELETE_ALL_EVENT";
 // operationReducer-Actions
 export const ADD_OPERATION_LOG = "ADD_OPERATION_LOG";
 export const DELETE_ALL_OPERATION_LOGS = "DELETE_ALL_OPERATION_LOGS";
+
+
+// themeReducer-actions
+export const CHANGE_LIGHT = "CHANGE_LIGHT";
+export const CHANGE_DARK = "CHANGE_DARK";
