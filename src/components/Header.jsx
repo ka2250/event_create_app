6b004288d095bd6d6f41618a70ac6a597c232a42
ka2250 <katok2250@gmail.com>
2021-01-21 11:46:28 +0900

    ヘッダーコンポーネント作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className="mt-5 d-flex justify-content-between align-items-center">
+            <div>
+                <h4>Creat Event App</h4>
+                <p>using react hooks.</p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
