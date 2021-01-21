e59ee5f756eda5e5a784dc9c95e74957440e4ec2
ka2250 <katok2250@gmail.com>
2021-01-21 11:45:58 +0900

    ログが表示されるコンポーネント作成

---
+++ src/components/OperationLog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Operationlog = ({ operationLog, index }) => {
+    return (
+        <tr>
+            <th className="text-center">{index + 1}</th>
+            <td>{operationLog.description}</td>
+            <td className="text-center">{operationLog.operatedAt}</td>
+        </tr>
+    );
+}
+
+export default Operationlog;
