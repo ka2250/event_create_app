feb6cc63ac5ae816e41f99d9198e143580b7dbe7
ka2250 <katok2250@gmail.com>
2021-01-27 00:02:27 +0900

    create ThemeContext, ThemeProvider

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,5 +1,45 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-const ThemeContext = createContext();
+const themes = {
+	light: {
+		background: "white",
+		color: "#2A2A2A",
+	},
+	dark: {
+		background: "#2A2A2A",
+		color: "white",
+	},
+};
 
-export default ThemeContext;
+const initialState = {
+	dark: false,
+	theme: themes.light,
+	toggle: () => {},
+};
+
+const ThemeContext = createContext(initialState);
+
+const ThemeProvider = ({ children }) => {
+	const [dark, setDark] = useState(false);
+
+	useEffect(() => {
+		const isDark = localStorage.getItem("dark") === true;
+		setDark(isDark);
+	}, [dark]);
+
+	const toggle = () => {
+		const isDark = !dark;
+		localStorage.setItem("dark", JSON.stringify(isDark));
+		setDark(isDark);
+	};
+
+	const theme = dark ? themes.light : themes.dark;
+
+	return (
+		<ThemeContext.Provider value={{ theme, dark, toggle }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+export { ThemeContext, ThemeProvider };
